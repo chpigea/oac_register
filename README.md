40dc58ce8b4d8b71bf9bba82448d8357246d352c
Christian R. Picone <ch.picone@gmail.com>
2025-10-11 15:18:41 +0200

    add environment variable

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # oac_register
 Register for the OAC project
+
+## ENV variables
+Env variables overwrites parameters from the config file
+
+### OAC_DB_USER
+Username for the OAC datbase
+
+### OAC_DB_PASSWORD
+Password for the OAC datbase
--- src/config.js
+++ src/config.js
@@ -7,6 +7,10 @@ function loadConfig() {
   const configFolder = process.env.OAC_CONFIG_FOLDER || __dirname;
   const configPath = path.join(configFolder, 'config', `${env}.json`);
   const config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
+
+  config.store.pg.user = process.env.OAC_DB_USER || config.store.pg.user;
+  config.store.pg.password = process.env.OAC_DB_PASSWORD || config.store.pg.password;
+
   return config;
 }
 
